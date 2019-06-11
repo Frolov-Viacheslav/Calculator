dbe0a15e959d835057b4cd39c0495122ea2f8cb6
developer <developer@com>
2019-06-11 12:55:51 +0000

    fix version conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello, I am developer");
-=======
-        System.out.println("Test hosts!");
->>>>>>> b807253e7112121d4cccf5164346865fe4b6570b
     }
 }
