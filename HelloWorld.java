e37b66c4e36e1bb3b6dcf048c4fe3b05a34fa00a
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2019-06-15 23:57:20 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Docker test");
+        System.out.println("Hello I am jenkins slave in Docker container");
     }
 }
