a2ed593ee143764d14bc293e14072c8c6fbd8c83
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2019-06-20 18:21:34 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Ansible to production");
+        System.out.println("Ansible to production !");
     }
 }
